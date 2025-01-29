fe0bfabb42965eb2432112ac616b9df7e4c51508
antoinedevillers <65911319+antoinedevillers@users.noreply.github.com>
2025-01-29 16:53:14 +0100

    Update connection.js

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:WOiZ5xKI5hInoQPt@cluster0.zklqg.mongodb.net/hackatweet1';
+const connectionString = '';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
